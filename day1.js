e2e2a45fa0444c3fcbf30fcba51fec1bc4cb1d5c
Matthew Powers-Freeling <mattpf@mit.edu>
2013-06-10 12:27:16 -0400

    Working calculator.

--- day1.js
+++ day1.js
@@ -1,11 +1,67 @@
 (function() {
-    var calculate = function() {
-        var input = $('#text1');
-        var val = input.val();
-        $('#output').text(val);
+    var operations = {
+        '*': function(a, b) { return a * b; },
+        '+': function(a, b) { return a + b; },
+        '-': function(a, b) { return a - b; },
+        '/': function(a, b) { return a / b; }
+    };
+    
+    var read_operand = function(tokens) {
+        var token = tokens.shift(1);
+        var number = parseInt(token);
+        if(number != number) {
+            throw "Expected an integer";
+        }
+        return number;
+    }
+    
+    var evaluate = function(tokens) {
+        if(tokens.length === 0) {
+            throw "Missing an operand";
+        }
+        var value = read_operand(tokens);
+        while(tokens.length > 1) {
+            var operator = tokens.shift(1);
+            var operand = read_operand(tokens);
+            value = operations[operator](value, operand);
+        }
+        return value;
+    }
+    
+    var calculate = function(text) {
+        var pattern = /(?:[+*\/()\-]|\d+)/g;
+        var tokens = text.match(pattern);
+        try {
+            var result = evaluate(tokens);
+            if(tokens.length) {
+                throw "ill-formed expression";
+            }
+            return String(result);
+        } catch(e) {
+            return e;
+        }
+    };
+    
+    var create_calculator = function(container) {
+        var form = $('<form>');
+        var field = $("<input type='text' size='50'>");
+        var button = $("<input type='submit' value='Calculate'>");
+        var output = $('<div>');
+        var p = $('<p>');
+        p.append(field, button);
+        form.append(p, output);
+        $(container).append(form);
+        
+        form.submit(function(e) {
+            e.preventDefault();
+            output.text(String(calculate(field.val())));
+            return false;
+        });
     }
     
     $(window).ready(function() {
-        $('#calculate').click(calculate);
+        $('.calculator').each(function() {
+            create_calculator(this);
+        });
     });
 })();
